43114e259a1753fa81f6e7a1526526e8ffdcd37f
andipartman-collab <andipartman@gmail.com>
2026-09-12 00:43:33 +0700

    Add Monitoring by SA menu

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,72 +4,44 @@ export function renderSidebar() {
     <aside class="sidebar">
 
       <div class="sidebar-header">
-
-        <div class="sidebar-logo">
-          SR
-        </div>
-
+        <div class="sidebar-logo">SR</div>
         <div class="sidebar-title">
           <strong>SOP Monitoring</strong>
           <span>Nasmoco Slamet Riyadi</span>
         </div>
-
       </div>
 
-
       <nav class="sidebar-menu">
-
-        <a
-          href="#"
-          class="menu-item"
-          data-page="dashboard"
-        >
+        <a href="#" class="menu-item" data-page="dashboard">
           <span class="menu-icon">⌂</span>
           <span>Dashboard</span>
         </a>
 
-
-        <a
-          href="#"
-          class="menu-item"
-          data-page="all-order"
-        >
+        <a href="#" class="menu-item" data-page="all-order">
           <span class="menu-icon">▤</span>
           <span>All Order</span>
         </a>
 
-
-        <a
-          href="#"
-          class="menu-item"
-          data-page="new-order"
-        >
+        <a href="#" class="menu-item" data-page="new-order">
           <span class="menu-icon">＋</span>
           <span>New Order</span>
         </a>
 
-
-        <a
-          href="#"
-          class="menu-item"
-          data-page="eta-update"
-        >
+        <a href="#" class="menu-item" data-page="eta-update">
           <span class="menu-icon">↻</span>
           <span>Update ETA</span>
         </a>
 
-
-        <a
-          href="#"
-          class="menu-item"
-          data-page="ata-update"
-        >
+        <a href="#" class="menu-item" data-page="ata-update">
           <span class="menu-icon">⇩</span>
           <span>Update ATA</span>
         </a>
 
+        <a href="#" class="menu-item" data-page="monitoring-sa">
+          <span class="menu-icon">👥</span>
+          <span>Monitoring by SA</span>
+        </a>
       </nav>
-
 
       <div class="sidebar-footer">
         <span>BO Monitoring</span>
