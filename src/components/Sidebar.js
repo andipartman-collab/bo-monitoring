78575113b54e970cb0590697583d1b3dc3734cc2
andipartman-collab <andipartman@gmail.com>
2026-09-15 23:42:02 +0700

    Use Toyota white logo variant in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,7 @@ export function renderSidebar() {
       <div class="sidebar-header">
         <div class="sidebar-logo">
           <img
-            src="https://brand.toyota.com/content/dam/brandhub/guidelines/logo/four-column/BHUB_Logo_ColorVariations_SingleColor_02.svg"
+            src="https://brand.toyota.com/content/dam/brandhub/guidelines/logo/two-column/BHUB_Logo_ColorVariations_Full-Color_02.svg"
             alt="Toyota"
           />
         </div>
