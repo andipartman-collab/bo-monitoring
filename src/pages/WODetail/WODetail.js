24ecbc6879101d75771903a60deccbabb8681356
andipartman-collab <andipartman@gmail.com>
2026-09-18 13:31:51 +0700

    Fix read-only WO detail back label

--- src/pages/WODetail/WODetail.js
+++ src/pages/WODetail/WODetail.js
@@ -281,3 +281,13 @@ function showError(messageText) {
     message.textContent = messageText
   }
 }
+
+
+function escapeHTML(value) {
+  return String(value ?? '')
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#039;')
+}
